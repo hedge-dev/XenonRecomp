988631a979aa87a8198a0ac96c0c5554ef57344f
harshit1879 <harshit1879.becse24@chitkara.edu.in>
2025-03-04 20:32:04 +0530

    Implemented user authentication module

---
+++ auth.py
@@ -0,0 +1,11 @@
+
+
+def login(username, password):
+
+    if username == "user" and password == "password":
+        return True
+    return False
+
+def register(username, password):
+
+    return f"User {username} registered successfully!"
