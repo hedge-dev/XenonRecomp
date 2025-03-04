5612497267280c1a047b3aec842d4438ed4ce829
harshit1879 <harshit1879.becse24@chitkara.edu.in>
2025-03-04 20:49:36 +0530

    update README with user autentication module info

--- auth.py
+++ auth.py
@@ -1,11 +1,3 @@
 
 
-def login(username, password):
-
-    if username == "user" and password == "password":
-        return True
-    return False
-
-def register(username, password):
-
-    return f"User {username} registered successfully!"
+print (" hello world ")
